f677c43a24ecf167bcce9351e09d3eaaf38cdf00
RitaDyvnych <m.dyvnych@gmail.com>
2021-10-09 18:16:57 +0300

    удалены лишние файлы из папок, добавлена базовая разметка инпута

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Phonebook</h1>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
